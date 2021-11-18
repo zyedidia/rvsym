ee98d74e209aed5257e8cca0858ef06901e36c08
Zachary Yedidia <zyedidia@gmail.com>
2021-11-18 15:22:04 -0800

    fix mem test

--- memory_test.go
+++ memory_test.go
@@ -1,35 +1,25 @@
 package rvsym
 
 import (
-	"fmt"
 	"testing"
 
 	"github.com/zyedidia/go-z3/st"
 )
 
 func TestMem(t *testing.T) {
-	mem := make(Memory)
+	mem := NewMemory(nil)
 	mem.Write32(0, st.Int32{C: 0x0A0B0C0D})
-	// mem.Write8(0, st.Int32{C: 0x0ff})
-	// mem.Write8(1, st.Int32{C: 0x0ff})
-	// mem.Write16(2, st.Int32{C: 0x0ffff})
 
-	if v, ok := mem.Read8(0); ok {
-		fmt.Printf("%x\n", v.C)
+	if v, ok := mem.Read8(0); !ok || v.C != 0xd {
+		t.Fatal("fail 0")
 	}
-	if v, ok := mem.Read8(1); ok {
-		fmt.Printf("%x\n", v.C)
+	if v, ok := mem.Read8(1); !ok || v.C != 0xc {
+		t.Fatal("fail 1")
 	}
-	if v, ok := mem.Read8(2); ok {
-		fmt.Printf("%x\n", v.C)
+	if v, ok := mem.Read8(2); !ok || v.C != 0xb {
+		t.Fatal("fail 2")
 	}
-	if v, ok := mem.Read8(3); ok {
-		fmt.Printf("%x\n", v.C)
+	if v, ok := mem.Read8(3); !ok || v.C != 0xa {
+		t.Fatal("fail 3")
 	}
-
-	// if v, ok := mem.Read32(0); !ok {
-	// 	t.Fatal("bad out of bounds")
-	// } else if uint32(v.C) != 0xffffffff {
-	// 	t.Fatal("bad", v)
-	// }
 }
